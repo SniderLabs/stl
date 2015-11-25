23d0dd65ef9011e893bc7e768becc8377ebbcab8
Silas Snider <swsnider@gmail.com>
2015-11-24 21:56:26 -0800

    initial import from older project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+[![Coverage](http://gocover.io/_badge/github.com/SniderLabs/stl?0)](http://gocover.io/github.com/SniderLabs/stl)
+[![Build Status](https://travis-ci.org/SniderLabs/stl.png?branch=master)](https://travis-ci.org/SniderLabs/stl)
+[![GoDoc](https://godoc.org/github.com/SniderLabs/stl?status.svg)](https://godoc.org/github.com/SniderLabs/stl)
+
 # stl
-STL Parser for Go
+
+STL Parser for Go. Understands both
---
+++ stl_test.go
@@ -0,0 +1,78 @@
+// Copyright 2013 Silas Snider. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package stl
+
+import "testing"
+
+func TestEmptyParse(t *testing.T) {
+	const input = ""
+	sf, err := ParseSTLBytes([]byte(input))
+	if err == nil {
+		t.Errorf("Expecting an error with 0-length STL.")
+	}
+	if sf != nil {
+		t.Errorf("Expecting nil SurfaceMesh if STL file is 0-length.")
+	}
+}
+
+func TestASCIIParse(t *testing.T) {
+	const input = `solid OpenSCAD_Model
+  facet normal 1 0 0
+    outer loop
+      vertex -2 0.5 -0.5
+      vertex -2 1.5 -0.5
+      vertex -2 1.5 0.5
+    endloop
+  endfacet
+  facet normal 1 0 -0
+    outer loop
+      vertex -2 0.5 0.5
+      vertex -2 0.5 -0.5
+      vertex -2 1.5 0.5
+    endloop
+  endfacet
+endsolid OpenSCAD_Model`
+	facets, err := ParseSTLBytes([]byte(input))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if len(facets) != 2 {
+		t.Fatalf("Wrong number of facets: got %d, expected 2", len(facets))
+	}
+	tv := Vertex{X: -2, Y: 0.5, Z: -0.5}
+	if facets[0].Vertices[0] != tv {
+		t.Fatalf("Incorrect parse. Got: %+v, expected %+v", facets[0].Vertices[0], tv)
+	}
+}
+
+func TestBinaryParse(t *testing.T) {
+	const input = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x80?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc0\x00\x00\x00?\x00\x00\x00\xbf\x00\x00\x00\xc0\x00\x00\xc0?\x00\x00\x00\xbf\x00\x00\x00\xc0\x00\x00\xc0?\x00\x00\x00?\x00\x00\x00\x00\x80?\x00\x00\x00\x00\x00\x00\x00\x80\x00\x00\x00\xc0\x00\x00\x00?\x00\x00\x00?\x00\x00\x00\xc0\x00\x00\x00?\x00\x00\x00\xbf\x00\x00\x00\xc0\x00\x00\xc0?\x00\x00\x00?\x00\x00"
+	facets, err := ParseSTLBytes([]byte(input))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if len(facets) != 2 {
+		t.Fatalf("Wrong number of facets: got %d, expected 2", len(facets))
+	}
+	tv := Vertex{X: -2, Y: 0.5, Z: -0.5}
+	if facets[0].Vertices[0] != tv {
+		t.Fatalf("Incorrect parse. Got: %+v, expected %+v", facets[0].Vertices[0], tv)
+	}
+}
+
+func TestNonStandardBinaryParse(t *testing.T) {
+	const input = "solid\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x80?\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc0\x00\x00\x00?\x00\x00\x00\xbf\x00\x00\x00\xc0\x00\x00\xc0?\x00\x00\x00\xbf\x00\x00\x00\xc0\x00\x00\xc0?\x00\x00\x00?\x00\x00\x00\x00\x80?\x00\x00\x00\x00\x00\x00\x00\x80\x00\x00\x00\xc0\x00\x00\x00?\x00\x00\x00?\x00\x00\x00\xc0\x00\x00\x00?\x00\x00\x00\xbf\x00\x00\x00\xc0\x00\x00\xc0?\x00\x00\x00?\x00\x00"
+	facets, err := ParseSTLBytes([]byte(input))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if len(facets) != 2 {
+		t.Fatalf("Wrong number of facets: got %d, expected 2", len(facets))
+	}
+	tv := Vertex{X: -2, Y: 0.5, Z: -0.5}
+	if facets[0].Vertices[0] != tv {
+		t.Fatalf("Incorrect parse. Got: %+v, expected %+v", facets[0].Vertices[0], tv)
+	}
+}
